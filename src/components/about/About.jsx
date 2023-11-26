6c3bcb75aba9ecdc7755dea803729b4b3fe6c201
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-11-25 19:32:06 -0600

    fixes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -115,7 +115,10 @@ const About = () => {
                     >
                         About our town
                     </h2>
-                    <span className='section__subtitle mb-8' id='spaces__text'>
+                    <span
+                        className='text-center block text-sm '
+                        id='spaces__text'
+                    >
                         Tequila it’s quaint town nestled in Jalisco, built with
                         cobblestone
                         <br />
@@ -141,12 +144,18 @@ const About = () => {
                 </div>
                 <div>
                     <h2
-                        className=' text-center text-[3.125rem] font-[900] text-black'
+                        className=' text-center text-[2.5rem] font-[900] text-black mt-5'
                         id='spaces__title'
                     >
-                        About our town
+                        What to do?
                     </h2>
-                    <span className='section__subtitle mb-8' id='spaces__text'>
+                    <p className='text-center font-[900] text-black'>
+                        Learn the artesanal process of out heritage
+                    </p>
+                    <span
+                        className=' text-center block text-sm'
+                        id='spaces__text'
+                    >
                         Tequila it’s quaint town nestled in Jalisco, built with
                         cobblestone
                         <br />
@@ -161,12 +170,77 @@ const About = () => {
                         <br />
                         woven into its vibrant tapestry
                     </span>
-                    <div className='flex flex-col md:flex-row justify-around w-5/6 h-4/6 mr-auto ml-auto md:gap-5 lg:gap-2 mt-10'>
+
+                    <div className='flex flex-col md:flex-row justify-around w-5/6 h-4/6 mr-auto ml-auto md:gap-5 lg:gap-2 mt-5'>
                         <img
-                            src={Agave}
-                            alt='Agave'
+                            src={Agave2}
+                            alt='Agave2'
                             className='w-[700px] object-cover'
-                            id='Agave'
+                            id='Agave2'
+                        />
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <p className='text-center font-[900] text-black'>
+                        Learn the artesanal process of out heritage
+                    </p>
+                    <span
+                        className=' text-center block text-sm'
+                        id='spaces__text'
+                    >
+                        Tequila it’s quaint town nestled in Jalisco, built with
+                        cobblestone
+                        <br />
+                        streets and the aroma of blue agave, a place where
+                        history and
+                        <br />
+                        tradition harmonize in every adobe wall. Its essence,
+                        distilled in the
+                        <br />
+                        warmth of hospitality, beckons travelers to savor the
+                        rich heritage
+                        <br />
+                        woven into its vibrant tapestry
+                    </span>
+
+                    <div className='flex flex-col md:flex-row justify-around w-5/6 h-4/6 mr-auto ml-auto md:gap-5 lg:gap-2 mt-5'>
+                        <img
+                            src={Agave3}
+                            alt='Agave3'
+                            className='w-[700px] object-cover'
+                            id='Agave3'
+                        />
+                    </div>
+                </div>
+                <div className='mt-5'>
+                    <p className='text-center font-[900] text-black'>
+                        Learn the artesanal process of out heritage
+                    </p>
+                    <span
+                        className=' text-center block text-sm'
+                        id='spaces__text'
+                    >
+                        Tequila it’s quaint town nestled in Jalisco, built with
+                        cobblestone
+                        <br />
+                        streets and the aroma of blue agave, a place where
+                        history and
+                        <br />
+                        tradition harmonize in every adobe wall. Its essence,
+                        distilled in the
+                        <br />
+                        warmth of hospitality, beckons travelers to savor the
+                        rich heritage
+                        <br />
+                        woven into its vibrant tapestry
+                    </span>
+
+                    <div className='flex flex-col md:flex-row justify-around w-5/6 h-4/6 mr-auto ml-auto md:gap-5 lg:gap-2 mt-5'>
+                        <img
+                            src={Agave4}
+                            alt='Agave4'
+                            className='w-[700px] object-cover'
+                            id='Agave4'
                         />
                     </div>
                 </div>
--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -9,7 +9,7 @@ const Location = () => {
                 <span className='section__subtitle text-[1rem]'>
                     Tequila, Jalisco
                 </span>
-                <h2 className=' -mt-10 md:-mt-0 text-center text-[3.125rem] font-[900] text-black'>
+                <h2 className=' -mt-12 md:-mt-0 text-center text-[3.125rem] font-[900] text-black'>
                     Our Location
                 </h2>
                 <span className='section__subtitle mb-8 text-black text-[1rem]'>
@@ -29,7 +29,7 @@ const Location = () => {
                         href='https://www.google.com/maps/dir//Hotel+Gloriagave,+Galeana+225,+La+Mezcalera,+46400+Tequila,+Jal./@20.8789636,-103.9250185,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x842615e7bd12aedd:0x7c34219aa579f58d!2m2!1d-103.8426168!2d20.8789833?hl=es-419&entry=ttu'
                         target='_blank'
                         rel='noreferrer'
-                        className='text-[1rem] items-center text-center ml-16 md:ml-0'
+                        className='text-[1rem] items-center text-center'
                     >
                         Open in maps
                         <i className='uil uil-angle-double-right text-right text-2xl hover:scale-105 transition ease-linear duration-[0.3s] align-baseline'></i>
