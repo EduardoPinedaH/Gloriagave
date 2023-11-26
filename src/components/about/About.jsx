49afdd2aba72d93cc6c33123e53520ddb92f1ac3
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-11-25 20:14:46 -0600

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -196,7 +196,7 @@ const About = () => {
                         Learn the artesanal process of out heritage
                     </p>
                     <span
-                        className=' text-left block text-[1rem] w-[90vw] mr-auto ml-auto'
+                        className=' text-left block text-[1rem] w-[85vw] mr-auto ml-auto'
                         id='spaces__text'
                     >
                         Tequila it’s quaint town nestled in Jalisco, built with
