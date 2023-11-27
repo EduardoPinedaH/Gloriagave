a36947f109bcda5928799a87c09f2c7b46cbe977
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-11-27 12:52:55 -0600

    Update Spaces.jsx

--- src/components/Spaces.jsx
+++ src/components/Spaces.jsx
@@ -3,13 +3,17 @@ import "../App.css";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Rooms from "./rooms/Rooms";
+import ScrollUp from "./scroll-up/ScrollUp";
 
 function Spaces() {
     return (
         <>
             <Header />
-            <Rooms />
+            <main className='main'>
+                <Rooms />
+            </main>
             <Footer />
+            <ScrollUp />
         </>
     );
 }
